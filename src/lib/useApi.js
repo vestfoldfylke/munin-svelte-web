ea9e5e95a56d1f982b3f5889bf484756af69b3f0
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-10 10:53:17 +0200

    chore: Refactor useApi.js for improved code readability and consistency

--- src/lib/useApi.js
+++ src/lib/useApi.js
@@ -6,7 +6,7 @@ export const getHuginToken = async (decoded) => {
   // MOCK access token for local api (the access token is just a demo token - nothing dangerous)
   // if (import.meta.env.VITE_MOCK_MSAL === 'true') return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGk6Ly9ibGFibGFiIiwiaXNzIjoiaHR0cHM6Ly9kdXN0LmR1c3Rlc2VuLnZ0ZmsubmV0L2hhaGFoLyIsImlhdCI6MTcwNjM2MDM5MiwibmJmIjoxNzA2MzYwMzkyLCJleHAiOjE3MDYzNjU4MjAsImFjciI6IjEiLCJhaW8iOiJiYWJhYmFiYWIiLCJhbXIiOlsicnNhIiwibWZhIl0sInJvbGVzIjpbImR1c3RfYWNjZXNzIiwiYWRtaW5fYWNjZXNzIl0sImFwcGlkIjoiZ3VkZW5lIHZlaXQiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IlNww7hrZWxzZSIsImdpdmVuX25hbWUiOiJEZW1vIiwiaXBhZGRyIjoiMjAwMToyMDIwOjQzNDE6ZmNiYjoyOTU5OjFjNmE6Y2RhYjoyNGUwIiwibmFtZSI6IkRlbW8gU3DDuGtlbHNlIiwib2lkIjoiMTIzNDUiLCJvbnByZW1fc2lkIjoiU1VTVVNVUyIsInJoIjoic2kgc2Vub3IiLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJtYXJpbmUiLCJ0aWQiOiJza2xlbW1lIiwidW5pcXVlX25hbWUiOiJkZW1vLnNwb2tlbHNlQHZlc3Rmb2xkZnlsa2Uubm8iLCJ1cG4iOiJkZW1vLnNwb2tlbHNlQHZlc3Rmb2xkZnlsa2Uubm8iLCJ1dGkiOiJob2hvbyIsInZlciI6IjEuMCJ9.64xzW92dVIXpZ_2OXQ6KQHITtYByDZJn1ycX3p_EkW4'
   let accessToken
-  if(!decoded) {
+  if (!decoded) {
     decoded = false
   }
   try {
@@ -16,14 +16,14 @@ export const getHuginToken = async (decoded) => {
       throw new Error('User not logged in yet - waiting for successful login')
     }
     accessToken = (await msalClient.acquireTokenSilent({ scopes: [import.meta.env.VITE_AI_API_SCOPE] })).accessToken
-    if(decoded) {
+    if (decoded) {
       // Return the decoded token
       const result = {
         upn: '',
         roles: []
       }
-      let decodedToken = jwtDecode(accessToken)
-      const { upn, roles} = decodedToken
+      const decodedToken = jwtDecode(accessToken)
+      const { upn, roles } = decodedToken
       result.upn = upn || 'appReg'
       result.roles = roles || []
 
@@ -39,6 +39,3 @@ export const getHuginToken = async (decoded) => {
     await login(true) // Sends the user back to main-page, so the search will have to be done again (this should not happen often)
   }
 }
-
-
-
