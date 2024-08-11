42de515ad6f144f2d5383d9d8a8c3db392b6b02f
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-11 10:55:22 +0200

    Refactor ChatBlobs component to have a css loading msg when waiting for a respons

--- src/lib/components/ChatBlobs.svelte
+++ src/lib/components/ChatBlobs.svelte
@@ -18,7 +18,7 @@
                 <p>{content}</p>
             {:else}
                 {#if content === "..."}
-                    <SvelteMarkdown source={ content } />
+                    <div class="loader"></div>
                 {:else}
                     <SvelteMarkdown source={ content } />
                 {/if}
@@ -56,5 +56,23 @@
     img {
         max-width: 400px;
         height: auto;
+    }
+
+    .loader {
+    width: 30px;
+    aspect-ratio: 2;
+    --_g: no-repeat radial-gradient(circle closest-side,#000 90%,#0000);
+    background: 
+        var(--_g) 0%   50%,
+        var(--_g) 50%  50%,
+        var(--_g) 100% 50%;
+    background-size: calc(100%/3) 50%;
+    animation: l3 1s infinite linear;
+    }
+    @keyframes l3 {
+        20%{background-position:0%   0%, 50%  50%,100%  50%}
+        40%{background-position:0% 100%, 50%   0%,100%  50%}
+        60%{background-position:0%  50%, 50% 100%,100%   0%}
+        80%{background-position:0%  50%, 50%  50%,100% 100%}
     }
 </style>
