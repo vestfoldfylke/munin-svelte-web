3213024f2fd880d39a5b3fe93b91f421d3ad4438
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-08-13 10:49:19 +0200

    refactor: Update model description in modelinfo.js

--- src/lib/data/modelinfo.js
+++ src/lib/data/modelinfo.js
@@ -2,7 +2,7 @@
 export const modelinfo = {
   option1: {
     navn: 'GPT-4o',
-    description: 'Denne modellen bruker GPT-4o til å generere tekst basert på en gitt kontekst og melding. GPT-4o er den nyeste versjonen av GPT-4, og er trent på en stor mengde data fra internett. Modellen er trent på å generere tekst som ligner på menneskelig skrevet tekst, og kan brukes til en rekke oppgaver som tekstgenerering, oversettelse og spørsmål-svar. Du må selv legge inn kontektst og temperatur slik at modellen får de egenskapene du ønsker. Denne har også mulighet for bildeanalyse. Trykk på tannhjulet og last opp et bilde. Modellen vil analysere bildet og generere tekst basert på dette.',
+    description: 'Denne modellen bruker GPT-4o til å generere tekst basert på en gitt kontekst og melding. GPT-4o er den nyeste versjonen av GPT-4, og er trent på en stor mengde data fra internett. Modellen er trent på å generere tekst som ligner på menneskelig skrevet tekst, og kan brukes til en rekke oppgaver som tekstgenerering, oversettelse og spørsmål-svar. Du må selv legge inn kontektst og temperatur slik at modellen får de egenskapene du ønsker. Denne modellen har også mulighet for bildeanalyse. Trykk på tannhjulet og last opp et bilde. Modellen vil analysere bildet og kan generere tekst basert på dette.',
     synligKontekst: true,
     illustrasjon: 'GPT4o.png'
   },
