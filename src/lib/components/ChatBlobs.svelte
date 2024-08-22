eaffd68332b861bdd840a7c66078b3c6d283e659
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-08-22 19:04:24 +0200

    refactor: Improve LaTeX expression handling and markdown conversion in ChatBlobs.svelte

--- src/lib/components/ChatBlobs.svelte
+++ src/lib/components/ChatBlobs.svelte
@@ -20,7 +20,8 @@
         // Replace all LaTeX expressions
         s = s.replace(/\\\[(.*?)\\\]/g, (_, match) => `$$${match}$$`);
         s = s.replace(/\\\((.*?)\\\)/g, (_, match) => `$${match}$`);
-        
+        s = s.replace(/\\\[\s*\n(.*?)\n\s*\\\]/s, (_, match) => `$$${match}$$`)
+        console.log(s)
         // Convert markdown to HTML
         let html = converter.makeHtml(s);
 
