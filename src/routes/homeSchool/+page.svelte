06c8263eb2f17b9ab91376f49e071d3eb3c7d638
Tom Jarle Christiansen <fuzzbin@gmail.com>
2024-09-19 07:45:57 +0200

    Remove admin restriction for 'Nora - Eksperimentell' option in model selection

--- src/routes/homeSchool/+page.svelte
+++ src/routes/homeSchool/+page.svelte
@@ -238,12 +238,7 @@
       <div class="boxyHeader">
         <select class="modellSelect" on:change={valgtModell}>
           <option value="option1" default>GPT-4o</option>
-          <!-- Skjuler Nora for alle som ikke har admin -->
-          {#if token.roles.includes(`${appName.toLowerCase()}.admin`)}
-            <option value="option2">Nora - Eksperimentell</option>
-          {:else}
-            <option value="option2" hidden>Nora - Eksperimentell</option>
-          {/if}
+          <option value="option2">Nora - Eksperimentell</option>
           <option value="option3">Matematikkens byggesteiner 1P og 2P</option>
           <option value="option4">Teoretisk matematikk 1T og R1</option>
           <option value="option5">Teoretisk matematikk R2</option>
