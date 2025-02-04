964ba0bf15b569882ddcf6851be9314132dbb127
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-02-04 21:43:54 +0100

    Add JWT decoding and update nbTranscript function to include user UPN; modify transcript output message

--- src/lib/services/openaiToolsLabs.js
+++ src/lib/services/openaiToolsLabs.js
@@ -4,6 +4,7 @@ import { params } from '$lib/data/modelparams'
 import { getHuginToken } from '../useApi'
 import { z } from "zod";
 import { zodResponseFormat } from "openai/helpers/zod";
+import { jwtDecode } from "jwt-decode";
 
 // Importere skjemaer fra eget bibliotek etterhvert
 const Superheltinfo = z.object({
@@ -45,12 +46,15 @@ export const testStructured = async (userParams) => {
 
 export const nbTranscript = async (filliste, metadata) => {
   console.log("Første fil: ", filliste)
+  const accessToken = await getHuginToken()
+  const user_upn = jwtDecode(accessToken).upn
+
   const datapakken = new FormData()
   datapakken.append('filer', filliste)
   datapakken.append('filnavn', metadata.filnavn)
   datapakken.append('spraak', metadata.spraak)
   datapakken.append('format', metadata.format)
-  const accessToken = await getHuginToken()
+  datapakken.append('upn', user_upn);
   console.log("lydfil: ", filliste)
   const r = await axios.post(`${import.meta.env.VITE_AI_API_URI}/nbTranscript`, datapakken, {
     method: 'post',
