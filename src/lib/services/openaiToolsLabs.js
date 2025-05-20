5e21d6855e9220b111f669b59f377191cc45534c
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-05-20 22:06:20 +0200

    Refactor: Remove unused schemas and test function from openaiToolsLabs.js

--- src/lib/services/openaiToolsLabs.js
+++ src/lib/services/openaiToolsLabs.js
@@ -1,47 +1,7 @@
 // Bibliotek for å håndtere API-kall til AZF-funksjoner
 import axios from 'axios'
-import { models } from '$lib/data/models'
 import { getHuginToken } from '../useApi'
-import { z } from "zod";
-import { zodResponseFormat } from "openai/helpers/zod";
 import { jwtDecode } from "jwt-decode";
-
-// Importere skjemaer fra eget bibliotek etterhvert
-const Superheltinfo = z.object({
-  navn: z.string(),
-  superkraft: z.string(),
-  kjønn: z.string(),
-  god: z.boolean(),
-});
-
-const MatteInfo = z.object({
-  tema: z.string(),
-  forklaring: z.string(),
-  nøkkelord: z.string(),
-});
-
-// Testfunksjon for å sjekke at skjemaene fungerer
-export const testStructured = async (userParams) => {
-  console.log(zodResponseFormat(Superheltinfo), "event")
-   // Template API-call
-   const payload = { ...models[12].params }  // Using model with id='12' for structured responses
-   payload.message = userParams.message
-   payload.messageHistory = userParams.messageHistory
-   payload.kontekst = userParams.kontekst
-   payload.temperatur = userParams.temperatur
-   payload.bilde_base64String = userParams.base64String
-   payload.response_format = zodResponseFormat(Superheltinfo, "SuperInfo")
-   payload.model = "gpt-4o-2024-08-06"
-
-   const accessToken = await getHuginToken()
-   // Call AZF-funksjon with payload
-   const response = await axios.post(`${import.meta.env.VITE_AI_API_URI}/structuredOpenAi`, payload, {
-     headers: {
-       authorization: `Bearer ${accessToken}`
-     }
-   })
-   return response
-}
 
 export const nbTranscript = async (filliste, metadata) => {
   const accessToken = await getHuginToken()
